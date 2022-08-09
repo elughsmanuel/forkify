d5bb6713e0e1828806f3d5d243ecdb3a5354698c
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-08-09 11:45:58 +0100

    Pagination

--- src/js/index.js
+++ src/js/index.js
@@ -28,7 +28,6 @@ const controlSearch = async () => {
         await state.search.getResults();
 
         // 5. Render results on UI
-        console.log(state.search.result);
         clearLoader();
         searchView.renderResults(state.search.result);
     }
@@ -38,3 +37,12 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+})
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,11 +3,12 @@ import { elements } from "./base";
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
-    elements.searchInput.value = '';
+  elements.searchInput.value = "";
 };
 
 export const clearResults = () => {
-    elements.searchResList.innerHTML = '';
+  elements.searchResList.innerHTML = '';
+  elements.searchResPages.innerHTML = '';
 };
 
 // 'Pasta with tomato and spinach'
@@ -18,19 +19,19 @@ export const clearResults = () => {
 // acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato']
 
 const limitRecipeTitle = (title, limit = 17) => {
-    const newTitle = [];
-    if(title.length > limit) {
-        title.split(' ').reduce((acc, cur) => {
-            if(acc + cur.length <= limit) {
-                newTitle.push(cur);
-            }
-            return acc + cur.length;
-        }, 0);
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
 
-        // return the result
-        return `${newTitle.join(' ')} ...`;
-    }
-    return title;
+    // return the result
+    return `${newTitle.join(" ")} ...`;
+  }
+  return title;
 };
 
 const renderRecipe = (recipe) => {
@@ -38,19 +39,64 @@ const renderRecipe = (recipe) => {
                     <li>
                         <a class="results__link" href="#${recipe.recipe_id}">
                             <figure class="results__fig">
-                                <img src="${recipe.image_url}" alt="${recipe.title}">
+                                <img src="${recipe.image_url}" alt="${
+    recipe.title
+  }">
                             </figure>
                             <div class="results__data">
-                                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
-                                <p class="results__author">${recipe.publisher}</p>
+                                <h4 class="results__name">${limitRecipeTitle(
+                                  recipe.title
+                                )}</h4>
+                                <p class="results__author">${
+                                  recipe.publisher
+                                }</p>
                             </div>
                         </a>
                     </li>
                 `;
-                elements.searchResList.insertAdjacentHTML('beforeend', markup);
+  elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
-export const renderResults = (recipes) => {
-    console.log(recipes);
-  recipes.forEach(renderRecipe);
+// type: 'prev' or 'next'
+
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
+
+const renderButton = (page, numResults, resPerPage) => {
+  const pages = Math.ceil(numResults / resPerPage);
+
+  let button;
+  if (page === 1 && pages > 1) {
+    // Only button to go to next page
+    button = createButton(page, 'next');
+  } else if (page < pages) {
+    // Both buttons
+    button = ` 
+        ${createButton(page, 'prev')}
+        ${createButton(page, 'next')}
+    `;
+  } else if (page === pages && pages > 1) {
+    // Only button to go to previous page
+    button = createButton(page, 'prev');
+  }
+
+  elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    // render results of current page
+//   console.log(recipes);
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  // render the pagination buttons
+  renderButton(page, recipes.length, resPerPage);
 };
